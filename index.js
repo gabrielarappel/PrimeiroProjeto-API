c0ceaae4d42f1428e56eac226f265fdaa2807de4
Gabriela Rappel <gabrielafr3005@gmail.com>
2025-10-05 23:11:56 -0300

    feat: criação de funções auxiliares para função depositar

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import chalk from "chalk";
 
 //modulos internos
 import fs from "fs";
+import { parse } from "path";
 
 operation();
 
@@ -96,6 +97,22 @@ function deposit() {
     if(!checkAccount(accountName)){
         return deposit()
     }
+
+    inquirer.prompt([
+        {
+            name: 'amount',
+            message: 'Quanto você deseja depositar',
+        }
+    ]).then((answer) => {
+
+        const amount = answer['amount']
+
+        //add an amount
+        addAmount(accountName, amount)
+        operation()
+
+    }).catch((err) => console.log(err))
+
 }).catch (err => console.log(err))
 }
 
@@ -105,4 +122,34 @@ function checkAccount(accountName){
         return false
     }
     return true
+}
+
+function addAmount(accountName, amount){
+
+    const accountData = getAccount(accountName)
+
+    if (!amount) {
+        console.log(chalk.bgRed.black('Ocorreu um erro, tente novamente mais tarde!'))
+        return deposit()
+    }
+
+    accountData.balance = parseFloat(amount) + parseFloat(accountData.balance)
+
+    fs.writeFileSync(
+        `accounts/${accountName}.json`,
+        JSON.stringify(accountData),
+        function(err) {
+            console.log(err)
+        },
+    )
+
+    console.log(chalk.green(`Foi depositado o valor de R$${amount} na sua conta!`))
+}
+
+function getAccount(accountName) {
+    const accountJSON = fs.readFileSync(`accounts/${accountName}.json`, {
+        encoding: 'utf8',
+        flag: 'r'
+    })
+    return JSON.parse(accountJSON)
 }
